617fb3d037dca4654da34d3d05cc26ce75983f15
karma9874 <33444666+karma9874@users.noreply.github.com>
2020-09-03 21:36:38 +0530

    fixed the action bar issue on splash screen

--- app/src/main/java/com/example/fire/splash.java
+++ app/src/main/java/com/example/fire/splash.java
@@ -1,41 +1,43 @@
 
 package com.example.fire;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
 
-        private static int SPLASH_SCREEN_TIME_OUT=2000;
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            //This method is used so that your splash activity
-            //can cover the entire screen.
-        setContentView(R.layout.activity_splash);
-
+            hideSystemUI();
+            setContentView(R.layout.activity_splash);
 
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    Intent i=new Intent(splash.this,
-                            login.class);
-                    //Intent is used to switch from one activity to another.
-
-                    startActivity(i);
-                    //invoke the SecondActivity.
-
+                    Intent intent1 = new Intent(getApplicationContext(), login.class);
+                    startActivity(intent1);
                     finish();
-                    //the current activity will get finished.
                 }
-            }, SPLASH_SCREEN_TIME_OUT);
+            }, 3000);
+
         }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
